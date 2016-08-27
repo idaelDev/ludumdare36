bbdc848aff774e66c274685d8955e420535d008e
Cheap Seth <cheapseth@electric-dragons.org>
2016-08-27 12:40:49 +0200

    Added preliminary gun stuff

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour 
+{
+
+	public float velocity = 20.0F;
+	public Vector2 direction = Vector2.up;
+
+	private new Transform transform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 translation = direction * velocity;
+		translation *= Time.deltaTime;
+
+		transform.Translate(translation.x, translation.y, 0);
+	}
+}
