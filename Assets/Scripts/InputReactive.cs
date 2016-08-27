9aca061804423aaac9a02a355a2ac34a9656ebc3
Cheap Seth <cheapseth@electric-dragons.org>
2016-08-27 11:53:31 +0200

    Added Player prefab

---
+++ Assets/Scripts/InputReactive.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputReactive : MonoBehaviour 
+{
+	public float movementSpeed = 10.0F;
+
+	private Transform transform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float horizontalTranslation = Input.GetAxis ("Horizontal") * movementSpeed;
+		float verticalTranslation = Input.GetAxis("Vertical") * movementSpeed;
+
+		horizontalTranslation *= Time.deltaTime;
+		verticalTranslation *= Time.deltaTime;
+
+		transform.Translate(horizontalTranslation, verticalTranslation, 0);
+	}
+}
