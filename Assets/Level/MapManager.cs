733b31e43bf908fe1305ed36511ad35b60c7eae9
Idael <judicael.abecassis@gmail.com>
2016-08-27 15:04:46 +0200

    Map Generation and draw

--- Assets/Level/MapManager.cs
+++ Assets/Level/MapManager.cs
@@ -3,23 +3,37 @@ using System.Collections;
 
 public class MapManager : MonoBehaviour {
 
-    Map map;
+    private Map map;
+    private GameObject[] rooms;
 
     // Use this for initialization
     void Start()
     {
         map = new Map(10);
+        rooms = Resources.LoadAll<GameObject>("levels");
     }
 
     void Update()
     {
         if(Input.GetKeyDown (KeyCode.B))
         {
-            map.mapGeneration();
-            foreach (Vector2 item in map.cellContainer.Keys)
-            {
-                Debug.Log(item);
-            }
+            Initmap();
+            //foreach (Vector2 item in map.cellContainer.Keys)
+            //{
+            //    Debug.Log(item);
+            //}
+        }
+    }
+
+    private void Initmap()
+    {
+        map.mapGeneration();
+        Debug.Log("Map Generated");
+        foreach(Cell item in map.cellContainer.Values)
+        {
+            Vector2 pos = new Vector2(item.Coordinates.x * 18, item.Coordinates.y * 10);
+            GameObject drawable = rooms[(int)(Random.Range(0, rooms.Length - 0.01f))];
+            Instantiate(drawable, pos, Quaternion.identity);
         }
     }
 
