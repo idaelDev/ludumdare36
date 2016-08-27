e70c7f4149e454129c638d187981cfe73db8155a
Idael <judicael.abecassis@gmail.com>
2016-08-27 10:38:45 +0200

    add [exemple]:
    event system exemple

---
+++ Assets/CodeExemple/EventListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventListener : MonoBehaviour {
+
+    EventSender sender;
+
+	// Use this for initialization
+	void Start () {
+        sender = GetComponent<EventSender>();
+        sender.inputEvent += EventTrigger;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void EventTrigger()
+    {
+        Debug.Log("Hey");
+    }
+}
