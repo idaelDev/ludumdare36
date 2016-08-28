eb66cb5aa6604b6f5058b545545017e4be756947
Idael <judicael.abecassis@gmail.com>
2016-08-28 14:42:39 +0200

    add menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public CanvasGroup main;
+    public CanvasGroup htp;
+    public CanvasGroup credits;
+
+    void Start()
+    {
+        goToMain();
+    }
+
+    public void activateAll(bool show)
+    {
+        activateCanvas(main, show);
+        activateCanvas(htp, show);
+        activateCanvas(credits, show);
+    }
+
+    public void activateCanvas(CanvasGroup c, bool show)
+    {
+        c.alpha = (show) ? 1 : 0;
+        c.blocksRaycasts = show;
+        c.interactable = show;
+    }
+
+    public void play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void goToMain()
+    {
+        activateAll(false);
+        activateCanvas(main, true);
+    }
+
+    public void goToCredits()
+    {
+        activateAll(false);
+        activateCanvas(credits, true);
+    }
+
+    public void goToHTP()
+    {
+        activateAll(false);
+        activateCanvas(htp, true);
+    }
+
+}
