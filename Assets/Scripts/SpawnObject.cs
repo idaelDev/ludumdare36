162ee8929b77a1b156a4dd7ddf129e18b7182bdc
Idael <judicael.abecassis@gmail.com>
2016-08-28 12:28:45 +0200

    Sprite importation + prefabs Decors create

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject : MonoBehaviour {
+
+    public GameObject objectToSpawn;
+    public bool isSpawned = false;
+
+    public void Spawn()
+    {
+        if (!isSpawned)
+        {
+            GameObject obj = Instantiate(objectToSpawn, transform.position, transform.rotation) as GameObject;
+            obj.transform.SetParent(this.transform);
+            isSpawned = true;
+        }
+    }
+}
